31bdf120aa508f0bf64c287e80fd4fb58a07b34c
pupupupupupupupupu <arsalanhassan327@gmail.com>
2026-02-11 09:56:27 +0530

    allowed origins

--- backend/src/config/allowedOrigins.js
+++ backend/src/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:5173",          // local Vite
   "http://localhost:3000",          // optional React
-  "https://your-frontend-domain.com" // production frontend
+  "https://shopcart-co.onrender.com" // production frontend
 ];
 
 module.exports = allowedOrigins;
